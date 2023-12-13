edb9adcccfc3383d7fc8c9eee5f5f1e09a43945e
vincent-gateau <vincentgateau@hotmail.fr>
2023-12-13 18:51:03 +0100

    setup projet

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section>
+      <h1 className="text-white">hello</h1>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nunc sed augue lacus
+        viverra vitae congue eu. Dignissim convallis aenean et tortor. Habitant
+        morbi tristique senectus et netus et malesuada. Nisl suscipit adipiscing
+        bibendum est. Non tellus orci ac auctor augue mauris augue neque
+        gravida. Sit amet purus gravida quis blandit turpis cursus. Dignissim
+        enim sit amet venenatis urna cursus eget. Aliquet nec ullamcorper sit
+        amet.
+      </p>
+    </section>
+  );
+};
+
+export default Navbar;
