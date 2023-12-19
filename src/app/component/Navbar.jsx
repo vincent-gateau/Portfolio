5866285b87708c5cb2024cd5b906a897f074a201
vincent-gateau <vincentgateau@hotmail.fr>
2023-12-19 22:29:54 +0100

    ajout template navbar et herosection

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -1,20 +1,66 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
+
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+];
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
-    <section>
-      <h1 className="text-white">hello</h1>
-      <p className="text-white">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Nunc sed augue lacus
-        viverra vitae congue eu. Dignissim convallis aenean et tortor. Habitant
-        morbi tristique senectus et netus et malesuada. Nisl suscipit adipiscing
-        bibendum est. Non tellus orci ac auctor augue mauris augue neque
-        gravida. Sit amet purus gravida quis blandit turpis cursus. Dignissim
-        enim sit amet venenatis urna cursus eget. Aliquet nec ullamcorper sit
-        amet.
-      </p>
-    </section>
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            >
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+    </nav>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import HeroSection from "./component/HeroSection";
 import Navbar from "./component/Navbar";
+import MenuOverlay from "./component/MenuOverlay";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-stone-900">
       <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
